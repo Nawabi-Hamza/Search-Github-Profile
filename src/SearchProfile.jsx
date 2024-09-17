e6d699a653b72b0a1dc6f51a562d60e0ec83efdc
Nawabi-Hamza <hamza.nawabi119@gmail.com>
2024-09-17 09:10:30 +0430

    update github profile search error handleing

--- src/SearchProfile.jsx
+++ src/SearchProfile.jsx
@@ -3,16 +3,20 @@ import SearchForm from './SearchForm'
 
 const BASE_URL = "https://api.github.com/users"
 function SearchProfile() {
-    const [ profile,setProfile ] = useState({ data:null , isLoadding:true , isEmpty:false})
+    const [ profile,setProfile ] = useState({ data:null , isLoadding:true })
     const [ username,setUsername ] = useState("Nawabi-Hamza")
-
+    const [ error,setError ] = useState(false)
     useEffect(function fetchPrfoile(){
         async function fetchGitHubProfile(){
             const response = await fetch(`${BASE_URL}/${username}`)
             const jsonResponse = await response.json()
             console.log(jsonResponse)
-            if(jsonResponse.message) return setProfile({ data:null, isLoadding:false, isEmpty:true })
-            setProfile({data:jsonResponse,isLoadding:false,isEmpty:false})
+            if(jsonResponse.message) {
+                setProfile({ data:null , isLoadding:false })
+                return setError(jsonResponse.message)
+
+            }
+            setProfile({data:jsonResponse,isLoadding:false })
         }
         fetchGitHubProfile()
     },[username])
@@ -22,11 +26,11 @@ function SearchProfile() {
         setUsername(val)
     }
   return (
-    <div>
+    <div>    
         <h2>GitHub Search Profile</h2>
         <SearchForm search={searchValue} />
         {profile.isLoadding && <h2>Loadding....</h2>}
-        {profile.isEmpty && <h2>User does not Exist try another username !</h2>}
+        {error && <h2>{error}</h2>}
         {profile.data && 
             <div className='Profile'>
                 <h2>{profile?.data?.login}</h2>    
